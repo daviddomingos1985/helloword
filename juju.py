703f16bc35fe090b2e93aff17e49983cd334bf0a
daviddomingos1985 <davidfabienebg@gmail.com>
2023-04-03 21:11:31 -0300

    primeiros programas em python

---
+++ juju.py
@@ -0,0 +1,8 @@
+print ('ola mundo eu sou a juju')
+
+nome = input ('Digite seu nome: ')
+sobrenome = input ('digite seu sobrenome: ')
+gosto = input ('digite aqui o que voce mais gosta: ')
+animal = input ('digite aqui o nome do seu animal: ')
+
+print ('Meu nome e',nome,'meu sobrenome e',sobrenome,'o que eu mais gosto e', gosto,'meu cachorro se chama:', animal)
